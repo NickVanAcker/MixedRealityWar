2d8d4e87afc98599c74f5cc0a0732c546e947639
JordiCoucke <s104623@ap.be>
2020-12-31 17:06:12 +0100

    effect shows up above objective

---
+++ Assets/Scripts/HighlightObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightObject : MonoBehaviour
+{
+    private GameObject[] Objectives;
+    public GameObject Effect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Objectives = GameObject.FindGameObjectsWithTag("Objective");
+        Effect = gameObject;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Objectives = GameObject.FindGameObjectsWithTag("Objective");
+        Vector3 ObjectPos = Objectives[0].transform.position;
+        ObjectPos = new Vector3(ObjectPos.x, ObjectPos.y + 0.4f, ObjectPos.z);
+        Effect.transform.position = ObjectPos;
+    }
+}
